25a61e2943f419a5746981890fd542561efa6436
Cameron <Cameron.Kolkey@gmail.com>
2021-09-23 19:00:02 +0200

    Update test_coverage_reporter.rb

--- lib/solargraph_test_coverage/test_coverage_reporter.rb
+++ lib/solargraph_test_coverage/test_coverage_reporter.rb
@@ -4,15 +4,22 @@
 module SolargraphTestCoverage
   class TestCoverageReporter < Solargraph::Diagnostics::Base
     include ReporterHelpers
+    include ReporterGuards
+    include DiagnosticMessages
 
     def diagnose(source, _api_map)
-      return [] if source.code.empty? || using_debugger?(source) || exclude_file?(source.location.filename)
-      return [test_missing_error(source)] unless File.file?(test_file(source))
+      return [] if source.code.empty? || using_debugger?(source) || exclude_file?(source) || is_test_file?(source)
+      return [test_missing_error(source)] unless has_test_file?(source)
 
-      results = run_test(source)
-      messages(source, results)
-    rescue ChildFailedError
-      []
+      results = run_test(source, FileHelpers.test_file(source))
+
+      [
+        line_warnings(source, results),
+        branch_warnings(source, results),
+        test_passing_error(source, results)
+      ].flatten.compact
+    rescue ChildFailedError => e
+      Config.debug? ? [debug_message(e, source)] : []
     end
   end
 end
